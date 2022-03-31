dd9b0c2db75c41ffdaf739d47821c06f5585fa5d
Noeingala <ingalanoelia@gmail.com>
2022-03-31 19:14:36 -0300

    homework4

--- 03-EstructuraDeDatos-I/homework/homework.js
+++ 03-EstructuraDeDatos-I/homework/homework.js
@@ -1,4 +1,7 @@
 'use strict'
+
+const { queue } = require("@11ty/eleventy-cache-assets")
+
 // Las funciones nFactoria y nFibonacci deben resolverlas
 // usando recursión. Una vez realizadas de esa forma pueden probar hacerlas
 // de forma iterativa pero esto último no es obligatorio.
@@ -7,7 +10,14 @@ function nFactorial(n) {
   // devolvé el factorial de n (n!)
   // ej:
   // el factorial de 3 es 6 (3 * 2 * 1)
+        if (n>-1 && n<2){ 
+          return 1
+        } else if (n < 0){ 
+          return 0
+        }
+         return n * nFactorial(n-1)
 }
+
 
 function nFibonacci(n) {
   // Secuencia de Fibonacci: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144,…
@@ -15,8 +25,14 @@ function nFibonacci(n) {
   // nFibonacci(0) // 0  // el elemento 0 es cero
   // nFibonacci(1) // 1 // el elemento 1 es 1
   // nFibonacci(6) // 1 // el elemento 6 es 8
-
+  if (n < 0){
+    return 0
+  } else if (n===1){
+    return 1
+  }
+    return nFibonacci(n-1) + nFibonacci(n-2)
 }
+
 
 // Para esta parte no es necesario utilizar recursión.
 // Implementa la clase Queue que debe contener los siguientes métodos:
@@ -25,8 +41,19 @@ function nFibonacci(n) {
 // size: Devuelve el número de elementos que contiene la queue.
 
 function Queue() {
-
+  this.queue = [];
 }
+ Queue.prototype.enqueue = function(elemento){
+   this.queue.push(elemento)
+}
+ Queue.prototype.dequeue = function(){
+   return this.queue.shift();
+ }
+ Queue.prototype.size = function(){
+   return this.queue.length
+}
+
+
 
 // No modifiquen nada debajo de esta linea
 // --------------------------------
